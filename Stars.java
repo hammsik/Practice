3da0f5c2fb15cdba9aae3a6f6619ae1940423e3b
hammsik <asbbrp@gmail.com>
2022-10-21 23:01:33 +0900

    dma

--- Stars.java
+++ Stars.java
@@ -1,7 +1,7 @@
 public class Stars {
     public static void main(String[] args) {
         for(int i=1; i<=10; i++) {
-            System.out.println("*".repeat(i));
+            System.out.println(" ".repeat(10-i) + "*".repeat(i));
         }
     }
 }
