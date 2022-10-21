3b95e40c2f2d156e0aa92406b1d89d2784c7d9f2
hammsik <asbbrp@gmail.com>
2022-10-21 23:21:33 +0900

    #

--- Stars.java
+++ Stars.java
@@ -1,7 +1,7 @@
 public class Stars {
     public static void main(String[] args) {
         for(int i=1; i<=20; i++) {
-            System.out.println(" ".repeat(20-i) + "\u203b".repeat(i));
+            System.out.println(" ".repeat(20-i) + "@".repeat(i));
         }
     }
 }
