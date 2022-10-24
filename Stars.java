e8ef0f653cf316cdb8a44ff3bdd7aaf4383ec642
hammsik <asbbrp@gmail.com>
2022-10-25 01:37:26 +0900

    "입력추가"

--- Stars.java
+++ Stars.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Stars {
     public static void main(String[] args) {
-        for(int i=1; i<=20; i++) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        for(int i=1; i<=n; i++) {
             System.out.println(" ".repeat(20-i) + "@".repeat(i));
         }
     }
